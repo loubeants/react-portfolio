413410b56db68a69e727a87d1a5e7a50fde6a9e0
Loubeants Celestin <loubeants@gmail.com>
2019-08-14 11:44:30 -0400

    some colors added

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function App() {
   return (<div className="demo-big-content">
   <Layout>
-      <Header title="Title" scroll>
+      <Header className='header-color' title="Title" scroll>
           <Navigation>
               <Link to ="/resume">Resume</Link>
               <Link to ="/aboutme">About Me</Link>
--- myportfolio/src/components/landingpage.js
+++ myportfolio/src/components/landingpage.js
@@ -1,10 +1,48 @@
 import React, {Component } from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 
 class Landing extends Component {
     render() {
         return(
-           <div><h1>Landing Page</h1></div> 
+            <div style={{Width: '100%', margin: 'auto'}}>
+            <Grid className='landing-grid'>
+                <Cell col={12}>
+                <img src='https://previews.123rf.com/images/jemastock/jemastock1708/jemastock170807747/83958935-dark-skin-man-with-afro-avatar-icon-image-vector-illustration-design.jpg'
+                alt='avatar'
+                className='avatar-img'
+                />
+
+
+                <div className='banner-text'>
+                <h1>Full Stack Web Developer</h1>
+
+                <hr/>
+
+                <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Python | Java | MySQL | MongoDB</p>
+
+                <div className='social-links'>
+
+                    {/*Linkedin*/}
+                    <a href='http://google.com' rel='noopener noreferrer' target='_blank'>
+                    <i className="fa fa-linkedin-square"  aria-hidden="true"/>
+                    </a>
+
+                    {/*GitHub*/}
+                    <a href='http://google.com' rel='noopener noreferrer' target='_blank'>
+                    <i className="fa fa-github-square"  aria-hidden="true"/>
+                    </a>
+
+
+
+
+                </div>
+                </div>
+                </Cell>
+
+            </Grid>
+
+            </div>
         )
     }
 }
