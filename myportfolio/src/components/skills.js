c15f87f372e652e2fe093df242446b19e7a1b2f1
Loubeants Celestin <loubeants@gmail.com>
2019-08-16 16:19:21 -0400

    resume page added

--- myportfolio/src/components/skills.js
+++ myportfolio/src/components/skills.js
@@ -5,7 +5,10 @@ class Skills extends Component {
     render() {
      return(
          <Grid>
-         <Cell col={12}> my col</Cell>
+         <Cell col={12}> 
+            <div style={{display: 'flex'}}>{this.props.skill}</div>
+         
+         </Cell>
          </Grid>
      )
   }
